7f689777084bf2bc6adb6582c9273720d6b2d5da
nbh880-web <nbh.880@gmail.com>
2026-03-30 12:30:42 +0300

    Update keys.js

--- api/keys.js
+++ api/keys.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    GEMINI: process.env.Gemini_API_KEY || process.env.GEMINI_API_KEY || null,
+    GEMINI: process.env.GEMINI_API_KEY || null,
     CLAUDE: process.env.CLAUDE_API_KEY || null,
     GPT: process.env.GPT_API_KEY || null,
     FIREBASE: process.env.FIREBASE_API_KEY || null
